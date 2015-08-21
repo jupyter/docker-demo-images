c945a516732703c6c0e69811feb36fc6703d5b65
Bill Sidney <bill@cognitivescale.com>
2015-08-21 22:35:57 +0000

    minor updates

--- fromJupyterDemo/setup_git_filters.sh
+++ fromJupyterDemo/setup_git_filters.sh
@@ -6,8 +6,8 @@
 # http://stackoverflow.com/questions/18734739/using-ipython-notebooks-under-version-control
 # upload ipynb_git_output_filter.py, setup_git_filters.sh, strip_output_notebook.py to 
 # https://s3.amazonaws.com/c12e-cognitivecloud/notebook/ipynb_git_output_filter.py etc
-wget https://s3.amazonaws.com/c12e-cognitivecloud/notebook/ipynb_git_output_filter.py
-wget https://s3.amazonaws.com/c12e-cognitivecloud/notebook/strip_output_notebook.py
+#wget https://s3.amazonaws.com/c12e-cognitivecloud/notebook/ipynb_git_output_filter.py
+#wget https://s3.amazonaws.com/c12e-cognitivecloud/notebook/strip_output_notebook.py
 git config filter.stripoutput.clean "$(git rev-parse --show-toplevel)/strip_output_notebook.py"
 git config filter.stripoutput.smudge cat
 git config filter.stripoutput.required true
