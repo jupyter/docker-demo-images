85d016a204b505bf87076de15d49ab39a972fd0e
kbroughton <kbroughton@cognitivescale.com>
2015-08-23 16:23:18 +0000

    relpath fixes

--- fromJupyterDemo/j2cli.sh
+++ fromJupyterDemo/j2cli.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-j2 Exercises/Sourcing/config/config-template.json > Exercises/Sourcing/config/config.json
-j2 Exercises/UI/config-template.yml > Exercises/UI/config.yml
+WORK_DIR=${1:-$COGU_HOME}
+
+pushd $WORK_DIR >> /dev/null
+j2 Exercises/Sourcing/config/config_tmpl.json > Exercises/Sourcing/config/config.json
+j2 Exercises/UI/config_tmpl.yml > Exercises/UI/config.yml
+popd
