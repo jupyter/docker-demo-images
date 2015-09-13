1f3514ce8bbb95450b73dfa49a9dde41050c99e7
Peter Parente <pparent@us.ibm.com>
2015-09-13 08:59:32 -0400

    Rebase on jupyter/minimal-notebook:4.0
    Contribution (c) Copyright IBM Corp. 2015

--- resources/jupyter_notebook_config.partial.py
+++ resources/jupyter_notebook_config.partial.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Configuration file for ipython-notebook.
-
-c = get_config()
-c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8888
 
 # Whether to trust or not X-Scheme/X-Forwarded-Proto and X-Real-Ip/X-Forwarded-
 # For headerssent by the upstream reverse proxy. Necessary if the proxy handles
